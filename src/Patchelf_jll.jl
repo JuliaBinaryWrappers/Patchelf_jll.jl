61964f2dc516b1f3f909219d45a24bcc4b082975
jlbuild <juliabuildbot@gmail.com>
2026-06-30 06:06:00 +0000

    Patchelf_jll build 0.18.1+0

--- src/Patchelf_jll.jl
+++ src/Patchelf_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Patchelf")
-JLLWrappers.@generate_main_file("Patchelf", UUID("f2cf89d6-2bfd-5c44-bd2c-068eea195c0c"))
+JLLWrappers.@generate_main_file("Patchelf", Base.UUID("f2cf89d6-2bfd-5c44-bd2c-068eea195c0c"))
 end  # module Patchelf_jll
